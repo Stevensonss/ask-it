767265ea1130a2f0d65072256a2933900e3f68b7
Glitch (hello-webpage) <none>
2023-02-19 16:54:29 +0100

    backend-switch-log-connecxion-incscription

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 const toggle = document.querySelector('.toggle');
 const header = document.querySelector('header');
 
+
 toggle.addEventListener("click", function() {
     header.classList.toggle("menu-toggle");
     toggle.classList.toggle('toggle-active')
 })
+
+const inscriptionLink = document.querySelector('.inscription>div>a:nth-child(1)');
+const connexionLink = document.querySelector('.connexion>div>a:nth-child(2)');
+
+const inscription = document.querySelector('.inscription');
+const connexion = document.querySelector('.connexion')
+
+function logSwitch() {
+    connexion.classList.toggle('log-hidden');
+    inscription.classList.toggle('log-hidden');
+}
+
+inscriptionLink.addEventListener("click", function() { logSwitch() });
+connexionLink.addEventListener("click", function() { logSwitch() });
